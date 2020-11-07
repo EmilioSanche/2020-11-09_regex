0dd428a4bb24058f6d470f2dc9404bfda66d9c6a
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2020-11-07 13:30:28 +0100

    first commit

---
+++ src/datos.js
@@ -0,0 +1,24 @@
+db.Motos.insertMany([
+    {"Tipo":"Deportiva", "Marca":"BMW", "Cilindrada":125, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Cross", "Marca":"Kawasaki", "Cilindrada":500, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Trail", "Marca":"BMW", "Cilindrada":300, "Gasolina":"95 octanos"}
+,
+    {"Tipo":"Trail", "Marca":"Victoria", "Cilindrada":125, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Scooter", "Marca":"Kawasaki", "Cilindrada":300, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Scooter", "Marca":"Suzuki", "Cilindrada":125, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Scooter", "Marca":"Victoria", "Cilindrada":125, "Gasolina":"95 octanos"}
+,
+    {"Tipo":"Deportiva", "Marca":"Kawasaki", "Cilindrada":300, "Gasolina":"95 octanos"}
+,
+    {"Tipo":"Scooter", "Marca":"Victoria", "Cilindrada":300, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Custom", "Marca":"Kawasaki", "Cilindrada":125, "Gasolina":"98 octanos"}
+,
+    {"Tipo":"Custom", "Marca":"BMW", "Cilindrada":125, "Gasolina":"95 octanos"}
+
+])
